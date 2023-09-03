9228bb4b556599bda01b068d99b909ab25a47182
snowlocked <19562649+snowlocked@users.noreply.github.com>
2023-09-03 20:36:52 +0800

    Update index.js

--- src/bilibili/index.js
+++ src/bilibili/index.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         删除B站无用节点
 // @namespace    http://github.com/snowlocked
-// @supportURL   https://github.com/snowlocked/README.md
-// @version      0.0.2
+// @version      0.0.1
 // @description  删除无用节点
 // @author       snowlocked
 // @license      MIT
@@ -12,20 +11,25 @@
 // ==/UserScript==
 
 
-(function () {
-  'use strict';
-  function remove() {
-    let extensionArea = document.querySelector('.bili-grid:has(.eva-extension-area)');
-    let battleArea = document.querySelector('.bili-grid:has(.battle-area)');
-    console.log('I want to remove extension and battle area', extensionArea, battleArea)
-    if (extensionArea && battleArea) {
-      extensionArea.remove();
-      battleArea.remove();
-    } else {
-      setTimeout(remove, 500)
+(function() {
+    'use strict';
+    function remove(){
+        const observer = new MutationObserver(function(mutations, observer){
+
+            let extensionArea = document.querySelector('.bili-grid:has(.eva-extension-area)');
+            let battleArea = document.querySelector('.bili-grid:has(.battle-area)');
+            console.log('remove',extensionArea, battleArea);
+            if(extensionArea || battleArea){
+                extensionArea?.remove();
+                battleArea?.remove();
+            }
+        });
+        var el = document.querySelector('.bili-layout');
+        const options = {
+            'childList': true,
+        } ;
+        observer.observe(el, options);
     }
 
-  }
-
-  window.addEventListener('DOMContentLoaded', remove)
+    window.addEventListener('DOMContentLoaded', remove)
 })();
